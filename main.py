2c674e228c7b0439a6720a4348dfcbb0cb706e13
Wendy Toledo <wendy.toledo.99@gmail.com>
2023-12-14 11:44:46 +0100

    modification du main et ajout des autres fonctions

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 import nltk
 import pdftotext
 from nltk.tag.stanford import StanfordNERTagger as NERTagger
-
+import re
+import os
 import subprocess
 import sys
-import os
 import shutil
 
 alpha = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN"
+num = "0123456789"
+numRom = "IVX"
 words = set(open("words.txt"))
 
 def getFileName(fileName):
@@ -108,15 +110,153 @@ def getAbstract(path: str):
     return abstract.strip()
 
 def getAutors(path):
-    return
+      with open(path, "rb") as f:
+        pdf = pdftotext.PDF(f)
+      # only first page
+      text = pdf[0]
+
+      indexFirstName = 1000
+      isAbtract = re.search(r"Abstract", text, re.I)
+      if isAbtract is None:
+        lines = text.splitlines()
+        indexAbstract = 0
+        for line in lines:
+            count = len(line)
+            if count>80:
+                break
+            indexAbstract += count + 1
+      else:
+        indexAbstract = isAbtract.start()
+      text = text[:indexAbstract]+"\n"+text[-1000:]
+
+      dictNames = {}
+      name_list = ["Minh", "Andrei", "Juan", "Daniel", "Bao", "Masaru", "Yi"]
+
+      # Solution works for any english name, but requires files english.all.3class.distsim.crf.ser.gz and stanford-ner.jar
+      st = NERTagger('stanford-ner/english.all.3class.distsim.crf.ser.gz', 'stanford-ner/stanford-ner.jar')
+
+      for sent in nltk.sent_tokenize(text):
+        tokens = nltk.tokenize.word_tokenize(sent)
+        tags = st.tag(tokens)
+        for tag in tags:
+            if tag[1] == 'PERSON': name_list.append(tag[0])
+
+      # automatically found names
+      for i, name in enumerate(name_list):
+        name_list[i] = ''.join([i for i in name if not (i.lower()).isdigit()])
+
+      # full names
+      for name in name_list:
+        index1 = text.find(name)
+        if 0 < index1 < indexFirstName:
+            indexFirstName = index1
+        if index1>=0:
+            index2 = index1
+            fullName = text[index1:index2].lower()
+            while not re.search(r'[,\n\\]|∗| and|\s{2,}', fullName):
+                index2 = index2 + 1
+                fullName = text[index1:index2]
+            foundName = re.sub(r'[0-9]| and|\\|∗|,|\n', ' ', fullName).strip()
+            if not (foundName in ' '.join(dictNames.keys())) and not any(name in foundName for name in dictNames.keys()):
+                dictNames[foundName] = None
+
+      mails = re.findall(r'[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+', text)
+      mailsWithList = re.findall(r'{[ a-zA-Z0-9.,-]+}@[a-zA-Z0-9.-]+', text)
+      if mailsWithList:
+        for mail in mailsWithList:
+            subStrings = re.split(r'[{}]', mail)
+            names = subStrings[1].split(',')
+            for name in names:
+                mails.append(name.strip() + subStrings[2])
+
+      for name in dictNames:
+        for k in range(1, 6):
+            for mail in mails:
+                substring_list = [name[i: j].lower() for i in range(len(name))
+                                   for j in range(i + 1, len(name) + 1)
+                                   if len(name[i:j]) == k]
+                if any(substring in mail.lower() for substring in substring_list):
+                    dictNames[name] = mail
+
+        if dictNames[name] is not None:
+            mails.remove(dictNames[name])
+
+      dictNames = {k: v for k, v in dictNames.items() if v}
+
+      AuthorsSection = text[indexFirstName:indexAbstract]
+      AuthorsSection = re.sub("\\\\|\[|]|∗|[ \t]{2,}", "", AuthorsSection)
+
+      return dictNames, AuthorsSection
     
+def getTextOnePara(path):
+    with open(path, "rb") as f:
+        pdf = pdftotext.PDF(f)
+
+    isAbtract = re.search(r"Abstract", pdf[0], re.I)
+    if isAbtract is None:
+        lines = pdf[0].splitlines()
+        indexAbstract = 0
+        for line in lines:
+            count = len(line)
+            if count > 80:
+                break
+            indexAbstract += count + 1
+    else:
+        indexAbstract = isAbtract.start()
+
+    # create text of first 3 pages
+    text = ""
+    for i in range(3):
+        page = pdf[i]
+        if i == 0:
+            page = page[indexAbstract:]
+        lines1 = re.split("\n", page)
+        text1para1 = text1para2 = ' '
+        for line in lines1:
+            if line != ' ':
+                para = re.split(r"[ \t]{8,}", line)
+                if len(para) == 1:
+                    firstpara = re.split(r"[ \t]{8,}", line)[0]
+                    text1para1 = text1para1 + firstpara.strip() + '\n'
+                if len(para) == 2:
+                    firstpara = re.split(r"[ \t]{8,}", line)[0]
+                    text1para1 = text1para1 + firstpara.strip() + '\n'
+                    secondpara = re.split(r"[ \t]{8,}", line)[1]
+                    text1para2 = text1para2 + secondpara.strip() + '\n'
+                if len(para) > 2:
+                    firstpara = re.split(r"[ \t]{8,}", line)[1]
+                    text1para1 = text1para1 + firstpara.strip() + '\n'
+                    secondpara = re.split(r"[ \t]{8,}", line)[2]
+                    text1para2 = text1para2 + secondpara.strip() + '\n'
+        text1para1 = re.sub(r"\n+", '\n', text1para1)
+        text1para2 = re.sub(r"\n+", '\n', text1para2)
+        text1 = text1para1 + ' ' + text1para2
+        text = text + '\n' + text1
+
+    return text
+  
+  
 def getIntro(path):
-    return
+
+    text = getTextOnePara(path)
+
+    indexIntro = re.search(r"Introduction|1[.]*[ \t]+[NTRODUCIntroduci]+[ \t]*", text, re.I)
+    if indexIntro is None:
+        indexIntro = re.search(r"Introduction|I[.]*[ \t]+[NTRODUCIntroduci]+", text, re.I).start()
+        indexSecondPara = indexIntro + re.search(r"\n[ \t]*II[.]*[ \t]+", text[indexIntro:], re.I).start()
+    else:
+        indexIntro = re.search(r"Introduction|1[.]*[ \t]+[NTRODUCIntroduci]+[ \t]*", text, re.I).start()
+        indexSecondPara = indexIntro + re.search(r"\n[ \t]*[.]*[ \t]+", text[indexIntro:], re.I).start()
+
+    Intro = text[indexIntro:indexSecondPara]
+    Intro = re.sub("\\\\|\[|]|∗|[ \t]{2,}", " ", Intro)
+
+    return Intro
     
 def getCorps(path):
-    return
+    return("")
     
-def getConclu(path: str):
+def getConclu(path):
     conclu = ""
 
     os.system("pdftotext -raw " + path + " tmp")
@@ -134,10 +274,10 @@ def getConclu(path: str):
         if len(line) == 0:
             continue
 
-        if "Conclusion" in line:
+        if "onclusion" in line:
             concluFound = True
 
-            index = line.find("Conclusion") + len(line)
+            index = line.find("onclusion") + len(line)
 
             while index < len(line):
                 if line[index] in alpha:
@@ -146,10 +286,10 @@ def getConclu(path: str):
 
                 index += 1
                 
-        if "Conclusions" in line:
+        if "onclusions" in line:
             concluFound = True
 
-            index = line.find("Conclusions") + len(line)
+            index = line.find("onclusions") + len(line)
 
             while index < len(line):
                 if line[index] in alpha:
@@ -158,10 +298,10 @@ def getConclu(path: str):
 
                 index += 1
                 
-        if "CONCLUSION" in line:
+        if "ONCLUSION" in line:
             concluFound = True
 
-            index = line.find("CONCLUSION") + len(line)
+            index = line.find("ONCLUSION") + len(line)
 
             while index < len(line):
                 if line[index] in alpha:
@@ -170,10 +310,10 @@ def getConclu(path: str):
 
                 index += 1
                 
-        if "CONCLUSIONS" in line:
+        if "ONCLUSIONS" in line:
             concluFound = True
 
-            index = line.find("CONCLUSIONS") + len(line)
+            index = line.find("ONCLUSIONS") + len(line)
 
             while index < len(line):
                 if line[index] in alpha:
@@ -181,11 +321,11 @@ def getConclu(path: str):
                     break
 
                 index += 1
-
-        if line[0] == 1 or "discussion" in line.lower() or "acknowledgements" in line.lower() or "acknowledgement" in line.lower():
+	
+        if line[0] == 1 or "discussion" in line.lower() or "discussions" in line.lower() or "references" in line.lower() or "acknowledgements" in line.lower() or "acknowledgement" in line.lower():
             break
 
-        if concluFound and "Conclusion" not in line  and "Conclusions" not in line and "CONCLUSION" not in line and "CONCLUSIONS" not in line:
+        if concluFound and "Conclusion" not in line and "Conclusions" not in line and "CONCLUSION" not in line and "CONCLUSIONS" not in line:
             conclu += line
 
         if line[-1] != "-":
@@ -201,9 +341,9 @@ def getConclu(path: str):
 
             if completeWord.lower() in words:
                 conclu = conclu.removesuffix("-")
-
+                
     return conclu.strip()
-    
+
 def getDiscu(path):
     discu = ""
 
@@ -222,10 +362,10 @@ def getDiscu(path):
         if len(line) == 0:
             continue
 
-        if "Discussion" in line:
+        if "Discussion" in line and line.find("Discussion")<20:
             discuFound = True
 
-            index = line.find("Discussion") + len(line)
+            index = line.find("DISCUSSION") + len(line)
 
             while index < len(line):
                 if line[index] in alpha:
@@ -234,7 +374,7 @@ def getDiscu(path):
 
                 index += 1
                 
-        if "DISCUSSION" in line:
+        if "DISCUSSION" in line and line.find("Discussion")<20:
             discuFound = True
 
             index = line.find("DISCUSSION") + len(line)
@@ -269,60 +409,102 @@ def getDiscu(path):
     return discu.strip()
        
 def getBiblio(path):
-    return
+     biblio = ""
+
+     os.system("pdftotext -raw " + path + " tmp")
+
+     f = open("tmp", "r")
+     corpus = f.readlines()
+     f.close()
+
+     os.remove("tmp")
+
+     biblioFound = False
+     for i in range(len(corpus)):
+        line = corpus[i].strip()
+
+        if len(line) == 0:
+            continue
+
+        if "references" in line.lower():
+            biblioFound = True
+            continue
+
+        if biblioFound and re.fullmatch(r'^[0-9]+$', line) == None:
+            biblio += line
+
+        if line[-1] != "-":
+            biblio += " "
+        else:
+            lastWordIndex = line.rfind(" ") + 1
+            lastWord = line[lastWordIndex:-1]
+
+            firstWordIndex = corpus[i+1].find(" ")
+            firstWord = corpus[i+1][:firstWordIndex]
+
+            completeWord = lastWord + firstWord + "\n"
+
+            if completeWord.lower() in words:
+                biblio = biblio.removesuffix("-")
+
+     return biblio.strip()
     
 if __name__ == '__main__':
-	#recupèration du nom du repertoire
-	arg = str(sys.argv[2:])
-	arg= arg.replace("[","")
-	arg= arg.replace("]","")
-	arg= arg.replace("'","")
-
-	#liste des fichiers du répertoire
-	listFile= os.listdir(arg)
 	
-	#suppression du repertoire de stockage s'il existe
-	pathResume = arg + "/articles_resumes"
-	if  os.path.exists(pathResume):
-		shutil.rmtree(pathResume)
+	if len(sys.argv) == 1:
+		print("La fonction doit s'appeler avec -t ou -x en argument pour avoir soit un fichier texte soit un fichier")
+	else:
+		print("Menu:")
+		print("1 Tout le répertoire")
+		print("2 PDF spécifiques")
+		choix = input("Entrer votre choix: ")
 		
-	#création du répertoire de stockage
-	os.mkdir(pathResume)
-	
-	#pour chaque fichier du répertoire, création d'un fichier texte du résumé dans le répertoire de stochage
-	for file in listFile:
-		if ".pdf" in file:
-			pathFile = arg +"/"+ file
+		if choix == "1":
 			
+			rep =input("Entrer les chemins du repertoir: ")
+			#liste des fichiers du répertoire
+			listFile = os.listdir(rep)
 			
-			if(str(sys.argv[1])=="-t"):
-				txtFile = file.replace(".pdf",".txt")
-				pathTxt = pathResume + "/" + txtFile
-				with open(pathTxt, 'w') as sys.stdout:
-					print(getFileName(file))
-					print(getTitle(pathFile))
-					print(getAutors(pathFile))
-					print(getAbstract(pathFile))
-					print(getIntro(pathFile))
-					print(getCorps(pathFile))
-					print(getConclu(pathFile))
-					print(getDiscu(pathFile))
-					print(getBiblio(pathFile))
-			
-			if(str(sys.argv[1])=="-x"):	
-				xmlFile = file.replace(".pdf",".xml")
-				pathXml = pathResume + "/" + xmlFile
-				with open(pathXml, 'w') as sys.stdout:
-					print("<article>")
-					print("		<preamble>",getFileName(file),"</preamble>")
-					print("		<titre>",getTitle(pathFile),"</titre>")
-					print("		<auteur>",getAutors(pathFile),"</auteur>")
-					print("		<abstract>",getAbstract(pathFile),"</abstract>")
-					print("		<introduction>",getIntro(pathFile),"</introduction>")
-					print("		<corps>",getCorps(pathFile),"</corps>")
-					print("		<conclusion>",getConclu(pathFile),"</conclusion>")
-					print("		<discussion>",getDiscu(pathFile),"</discussion>")
-					print("		<biblio>",getAutors(pathFile),"</biblio>")
-					print("</article>")
+		elif choix == "2":
+			#liste des fichiers à traiter
+			listFile = input("Entrer les chemins de vos fichier: ")
+		
+		if choix == "1" or choix == "2":
+			pathResume = sys.argv[2]
+			#pour chaque fichier du répertoire, création d'un fichier texte du résumé dans le répertoire de stochage
+			for file in listFile:
+				if ".pdf" in file:
+					if choix == "1":
+						pathFile = rep +"/"+ file
+					elif choix == "2":
+						pathFile =file
 					
-			
+					if(str(sys.argv[1])=="-t"):
+						txtFile = file.replace(".pdf",".txt")
+						pathTxt = pathResume + "/" + txtFile
+						with open(pathTxt, 'w') as sys.stdout:
+							print(getFileName(file))
+							print(getTitle(pathFile))
+							print(getAutors(pathFile))
+							print(getAbstract(pathFile))
+							print(getIntro(pathFile))
+							print(getCorps(pathFile))
+							print(getConclu(pathFile))
+							print(getDiscu(pathFile))
+							print(getBiblio(pathFile))
+					
+					if(str(sys.argv[1])=="-x"):	
+						xmlFile = file.replace(".pdf",".xml")
+						pathXml = pathResume + "/" + xmlFile
+						with open(pathXml, 'w') as sys.stdout:
+							print("<article>")
+							print("		<preamble>",getFileName(file),"</preamble>")
+							print("		<titre>",getTitle(pathFile),"</titre>")
+							print("		<auteur>",getAutors(pathFile),"</auteur>")
+							print("		<abstract>",getAbstract(pathFile),"</abstract>")
+							print("		<introduction>",getIntro(pathFile),"</introduction>")
+							print("		<corps>",getCorps(pathFile),"</corps>")
+							print("		<conclusion>",getConclu(pathFile),"</conclusion>")
+							print("		<discussion>",getDiscu(pathFile),"</discussion>")
+							print("		<biblio>",getAutors(pathFile),"</biblio>")
+							print("</article>")				
